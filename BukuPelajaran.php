4a97474d5d4b3fc8e01bbc7b42846afdd538985c
Muhammad Hisyam Yassar Akbar <muhammadhisyamyassar@gmail.com>
2025-12-11 06:30:50 +0700

    Update BukuPelajaran.php

--- BukuPelajaran.php
+++ BukuPelajaran.php
@@ -175,7 +175,7 @@
 
    </section>
     <div  class="copyright">
-        <p> &copy 2024 Man satu Sinjai by nr_faiqtunnis</p>
+        <p> &copy 2025 Man satu Sinjai by hisyam_yassar</p>
     </div>
   
 
